01964384c54ec653969a3a15309b4201b7bcd0bb
chee <yay@chee.party>
2025-01-20 16:00:55 +0000

    fix path in ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -23,7 +23,7 @@ module.exports = {
 			path: "/srv/www/cherries/",
 			"pre-deploy-local": "echo yay",
 			"post-deploy":
-				"npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+				"npm install && npm run build && pm2 reload ecosystem.config.cjs --env production",
 			"pre-setup": "echo yay",
 		},
 	},
