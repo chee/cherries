b87a161ab9551f2dfc729b4c9051a18c95151db1
chee <yay@chee.party>
2025-01-21 06:23:21 +0000

    try so hard

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -23,7 +23,7 @@ module.exports = {
 			path: "/srv/www/cherries/",
 			"pre-deploy-local": "echo yay",
 			"post-deploy":
-				"npm install && npm run build -- --outDir=new && mv dist old && mv new dist && mv old/astro.db new && rm -r old && pm2 reload ecosystem.config.cjs --env production",
+				"npm install && npm run build -- --outDir=new && rm -rf old && mv dist old && mv new dist && mv old/astro.db new && rm -r old && pm2 reload ecosystem.config.cjs --env production",
 			"pre-setup": "echo yay",
 		},
 	},
